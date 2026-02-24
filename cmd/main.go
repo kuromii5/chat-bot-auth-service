a7245130bcf8c1032caa1568c7b6d36b5d7373a7
kuromii5 <aleksandrstepanov771@gmail.com>
2026-02-24 03:18:52 +0300

    add return statement

--- cmd/main.go
+++ cmd/main.go
@@ -56,6 +56,7 @@ func main() {
 		if closeErr := pg.DB.Close(); closeErr != nil {
 			logrus.WithError(closeErr).Error("Database close failed")
 		}
+		return
 	case <-ctx.Done():
 		logrus.Info("Server shutdown...")
 
